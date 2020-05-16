bfec2386c49c1b2731ccae7eb35aacd510276185
baranovrp <baranov.r.p@gmail.com>
2020-05-16 19:44:38 +0200

    CSAA2020-07 Create one instance in public and one in private

--- chapter07vpc/main.tf
+++ chapter07vpc/main.tf
@@ -4,13 +4,39 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+data "aws_availability_zones" "available" {
+  all_availability_zones = true
+}
+
+data "aws_ami" "ami2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+data "aws_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+  name   = "default"
+}
+
 resource "aws_key_pair" "deployer" {
   key_name   = "atlantis"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfObcpiUJAYEGXnJ0FOcyTM6pFvs1tTFKhpuNWfE/sssk7oGnM2Kw3zdktg7Ykq/LV+tOlxl9VtBa9FN6BQmxMi/bW96c47rGYL8VMPCQ3e7Qa7mKjbx1coBcQg9gxaLpWA73oD41O2cHYit084SlS8BTiRl1f4Lc9nPKM9RKyOzC6zajyIBFLDjOcRgVkEVoEW8QYroAFLJwKuKqu9oI9HAuov0c1o99J4ASqKmC/rm/76d1Fhs83dXNhLldmme7aN7M7XKX+8NM7hPeJtG3LGuxOtVMmMOhPkqG7FbtFWhKuXvD5CdU/S7QkxGo3lkZE+cwrUqKWQmEB6t4lKkxB"
-}
-
-data "aws_availability_zones" "available" {
-  all_availability_zones = true
 }
 
 resource "aws_vpc" "this" {
@@ -72,4 +98,80 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "this" {
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public.id
+}
+
+resource "aws_instance" "public" {
+  ami           = data.aws_ami.ami2.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [
+    aws_security_group.ssh.id,
+    aws_security_group.web.id,
+    aws_security_group.world.id
+  ]
+  key_name                    = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "acloudguru public ec2"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                         = data.aws_ami.ami2.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private.id
+  security_groups             = [data.aws_security_group.default.id]
+  key_name                    = aws_key_pair.deployer.key_name
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "acloudguru private ec2"
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name   = "ssh"
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  security_group_id = aws_security_group.ssh.id
+
+  from_port   = 22
+  to_port     = 22
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "web" {
+  name   = "web"
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "allow_web" {
+  type              = "ingress"
+  security_group_id = aws_security_group.web.id
+
+  from_port   = 80
+  to_port     = 80
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "world" {
+  name   = "world-out"
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "allow_all_out" {
+  type              = "egress"
+  security_group_id = aws_security_group.world.id
+
+  from_port        = 0
+  to_port          = 0
+  protocol         = -1
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
 }
